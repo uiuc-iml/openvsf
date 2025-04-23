6938b26e12e5b8922b2def41f0190958898a98ac
Shaoxiong Yao <syao16@illinois.edu>
2025-04-22 22:48:44 -0500

    fix prior_factory reshape features to 2D function, add docs and rename contextualprior to conditioanldistribution

--- vsf/prior/prior_factory.py
+++ vsf/prior/prior_factory.py
@@ -2,14 +2,22 @@ import torch
 from ..core.base_vsf import BaseVSF
 from ..core.point_vsf import PointVSF
 from .distribution import ParamDistribution,DiagGaussianDistribution
-from .prior import LearnableContextualPrior, TorchLearnConfig
+from .conditional_distribution import LearnableConditionalDistribution, TorchLearnConfig
 from abc import ABC,abstractmethod
 from typing import List,Dict
 
 
 class BaseVSFPriorFactory(ABC):
-    """A factory class that creates a prior distribution for the VSF
+    r"""A factory class that creates a prior distribution for the VSF
     material parameters.
+    
+    This class behaves like the meta-prior defined in the SBML CoRL 2024 paper.
+    This class uses features saved in the VSF class to predict a distribution 
+    of the material parameters.
+    
+    Mathematically, this class is the P(\theta_\alpha|\psi, S_\alpha) in the paper.
+    Note that this class will not assume structures of the stiffness distribution, 
+    it will provide a plain 
     """
     @abstractmethod
     def predict(self, vsf : BaseVSF,
@@ -22,7 +30,7 @@ class BaseVSFPriorFactory(ABC):
 
         Future implementations may include a way to specify which material
         parameters to predict.  Current implementations just predict
-        stiffness
+        stiffness of VSFs.
         """
         raise NotImplementedError()
         
@@ -33,7 +41,12 @@ class BaseVSFPriorFactory(ABC):
     
     @abstractmethod
     def state_dict(self) -> dict:
-        """Returns all the parameters of the prior as a dictionary."""
+        """
+        Returns all the parameters of the prior as a dictionary.
+        
+        NOTE: different from `parameters()` that returns reference to 
+        internal parameters, this function returns a copy of the parameters.
+        """
         raise NotImplementedError()
 
 
@@ -64,14 +77,27 @@ class LearnableVSFPriorFactory(BaseVSFPriorFactory):
 
     The predict_torch function must be differentiable through torch.
     """
-    def __init__(self, feature_keys:List[str], prior : LearnableContextualPrior):
+    def __init__(self, feature_keys:List[str], prior : LearnableConditionalDistribution):
         self.feature_keys:List[str] = feature_keys
-        self.prior:LearnableContextualPrior = prior
+        self.prior:LearnableConditionalDistribution = prior
         self.learn_config = TorchLearnConfig()
 
-    def predict(self, vsf:BaseVSF, features:Dict[str,torch.Tensor]=None, material_param='stiffness') -> ParamDistribution:
-        """Predicts the material parameters of the given VSF."""
-        context = self.feature_tensor(vsf,features,self.feature_keys)
+    def predict(self, vsf:BaseVSF, features:Dict[str,torch.Tensor]=None, 
+                material_param='stiffness') -> ParamDistribution:
+        """
+        Predicts the material parameters of the given VSF.
+        Optionally only predicts for a subset of the points using the subset_indices.
+        
+        Args:
+            vsf (BaseVSF): The VSF to predict the material parameters for.
+            features (Dict[str,torch.Tensor]): Extra features to use for the prediction.
+            material_param (str): The material parameter to predict. Defaults to 'stiffness'.
+        
+        Returns:
+            params_distribution (ParamDistribution): The predicted distribution of material parameters.
+
+        """
+        context = self.feature_tensor(vsf,features)
         return self.prior.predict(context)
 
     def meta_learn(self, vsfs : List[BaseVSF],
@@ -101,6 +127,17 @@ class LearnableVSFPriorFactory(BaseVSFPriorFactory):
         """Returns the torch parameters that can be optimized, i.e., same as
         torch.nn.Module.parameters()."""
         return self.prior.parameters()
+    
+    def state_dict(self) -> dict:
+        """Returns all the parameters of the prior as a dictionary.
+        NOTE: different from `parameters()` that returns reference to 
+        internal parameters, this function returns a copy of the parameters.
+        """
+        return self.prior.state_dict()
+
+    def load_state_dict(self, state_dict:dict) -> None:
+        """Sets the parameters of the prior."""
+        self.prior.load_state_dict(state_dict)
 
     def to(self, device):
         """Moves the meta prior to the given device / dtype and return self."""
@@ -123,11 +160,22 @@ class LearnableVSFPriorFactory(BaseVSFPriorFactory):
         """
         assert isinstance(vsf, PointVSF)
         assert len(self.feature_keys) > 0
-        selected = [features[key] if key in features else vsf.features[key] for key in self.feature_keys]
-        selected_reshaped = []  #make 2D tensors
+
+        # Get features from external features or from vsf.features
+        selected = []
+        for key in self.feature_keys:
+            if features is not None and key in features.keys():
+                selected.append(features[key])
+            else:
+                selected.append(vsf.features[key])
+        
+        # Reshape the features to be 2D tensors
+        selected_reshaped = [] 
         for f in selected:
-            assert f.shape[0] == selected.shape[0]
-            selected_reshaped.append(f.reshape(selected.shape[0],-1))
+            assert f.shape[0] == vsf.num_points
+            selected_reshaped.append(f.reshape(vsf.num_points,-1))
+        
+        # Return the concatenated tensor
         return torch.cat(selected_reshaped,dim=1)
 
     def feature_tensor_batch(self, vsfs : List[BaseVSF], features : List[Dict[str,torch.Tensor]]) -> torch.Tensor:
